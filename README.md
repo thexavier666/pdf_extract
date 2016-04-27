d7e61d2e9006c210c061b8b06bfe722e5623d4bc
thexavier666 <sumitros@gmail.com>
2016-04-28 02:19:26 +0530

    Made a PDF data extractor

---
+++ README.md
@@ -0,0 +1,14 @@
+##PDF Extracter
+
+####About
+It extracts the content of a given PDF file and dumps it into an XML file.
+It is recommended to use jQuery to extract the data
+
+####Prerequisites
+ - Install `python`
+ - Install the Python Package Manager`pip` via the following command (Linux only as far as I know) -> `sudo apt-get install python-pip`
+ - Install the `pdfquery` module via the following command -> `pip install pdfquery`
+
+####Usage
+ - Run the following command -> `python pdf_extract <file_name>.pdf <output_file>`
+ - Note that there is no need to put .xml extension in output file name
---
+++ pdf_extract.py
@@ -0,0 +1,30 @@
+# Used for command line arguments
+import sys
+
+# Used for extracting PDF contents
+import pdfquery
+
+def main():
+	# Number of arguments must be 2
+	# Otherwise error will be thrown
+	if len(sys.argv) < 2:
+		print "Usage : python pdf_extract <file_name>.pdf <output_file>"
+		print "Note  : XML file will be created. No need to put .xml extension in output file name"
+
+	else:
+		# Takes the PDF file from command line argument
+		pdf = pdfquery.PDFQuery(sys.argv[1])
+
+		# Loads page 0 into memory
+		# Use pdf.load() to load entire file
+		# Use pdf.load(1,3,5) to load select pages
+		pdf.load(0)
+
+		# Outputs the entire contents into output file
+		# Use jQuery to extract data
+		pdf.tree.write(str(sys.argv[2] + ".xml"),pretty_print=True)
+
+	return 0
+
+if __name__ == '__main__':
+	main()
